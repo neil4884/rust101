2d2b49ea94ae5e204a25d97f57619937ae4d0931
vivatsathornthitasirivit <neil4884@gmail.com>
2022-12-07 13:35:33 +0700

    Initial commit

---
+++ src/a01_variables.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let mut x = 4;
+    let y = 8;
+
+    println!("{}", x);
+
+    x = 8;
+
+    println!("{x}");
+}
---
+++ src/main.rs
@@ -0,0 +1,10 @@
+mod a01_variables;
+mod a02_data_types;
+
+fn main() {
+    // a01_variables::a01();
+    // println!();
+    // a01_variables::a02();
+    // println!();
+
+}
